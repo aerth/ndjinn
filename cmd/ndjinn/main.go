1f0d35c7225d3e1e0d5d9c395e826cdfb52ab7df
aerth <aerth@isupon.us>
2016-03-30 08:50:24 -0700

    Add some e handling nad fix up log switcher

--- cmd/ndjinn/main.go
+++ cmd/ndjinn/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"math/rand"
 	"log"
 	"os"
 	"runtime"
@@ -46,26 +47,44 @@ func main() {
 
 	// Help Mode
 	if len(os.Args) > 1 {
-		fmt.Println("Usage:	")
+		fmt.Println("Usage:	Coming Soon")
 		os.Exit(0)
 	}
 
-	// Switch to log file
-	/*
-		if !InitLog() {
-			panic("Log Error")
-		}
-	*/
+fmt.Printf("Boot: Started (%s)\n",time.Now().Format("2006-01-02 15:04:05"))
 
 	// Load the configuration file (./config/config.json)
+	fmt.Printf("Boot: Loading config.")
+	fmt.Printf(".")
+	fmt.Printf(".")
+	fmt.Printf(".")
 	jsonconfig.Load("config"+string(os.PathSeparator)+"config.json", config)
+	fmt.Printf(" done.\n")
 
 	// Configure the session cookie store
+	fmt.Printf("Boot: Loading cookie store.")
+	fmt.Printf(".")
+	fmt.Printf(".")
+	fmt.Printf(".")
 	session.Configure(config.Session)
-	checkout.Configure(config.Checkout)
+	fmt.Printf(" done.\n")
 
-	// Connect to database (./database.db)
+	fmt.Printf("Boot: Loading PayPal.")
+	fmt.Printf(".")
+	fmt.Printf(".")
+	fmt.Printf(".")
+	checkout.Configure(config.Checkout)
+	fmt.Printf(" done.\n")
+
+	// Connect to database (./database/database.db)
+
+	fmt.Printf("Boot: Loading database.")
+	fmt.Printf(".")
+	_ = os.Mkdir("backups", 0700)
+	fmt.Printf(".")
+	fmt.Printf(".")
 	database.Connect(config.Database)
+	fmt.Printf(" done.\n")
 
 	// Run Maint and Backup
 	_, err := maint.RunSchedule()
@@ -77,7 +96,15 @@ func main() {
 		fmt.Println(err)
 	}
 	database.Connect(config.Database)
-	log.Println("Database Backup completed.")
+	fmt.Println("Boot: Database Backup completed.")
+
+	err = os.Mkdir("logs", 0700)
+	if err != nil && err.Error() != "mkdir logs: file exists" {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Boot: Logging requests to ./logs/access.log")
+	fmt.Println("Boot: Other logging at ./logs/debug.log")
 
 	// // // //
 
@@ -85,20 +112,49 @@ func main() {
 	recaptcha.Configure(config.Recaptcha)
 
 	// Setup the views (./template and ./static)
+	fmt.Printf("Boot: Loading views.")
+	fmt.Printf(".")
+	fmt.Printf(".")
+	fmt.Printf(".")
 	view.Configure(config.View)
+	fmt.Printf(" done.\n")
+
+	fmt.Printf("Boot: Loading templates.")
+	fmt.Printf(".")
+	fmt.Printf(".")
+	fmt.Printf(".")
 	view.LoadTemplates(config.Template.Root, config.Template.Children)
+	fmt.Printf(" done.\n")
+
+	fmt.Printf("Boot: Loading view plugins.")
+	fmt.Printf(".")
+	fmt.Printf(".")
+	fmt.Printf(".")
 	view.LoadPlugins(
 		plugin.TagHelper(config.View),
 		plugin.NoEscape(),
 		recaptcha.RecaptchaPlugin())
 
+	fmt.Printf(" done.\n")
+
 	// Monitor Uptime
 	go func() {
 		for {
-			time.Sleep(5 * time.Minute)
+			amt := time.Duration(rand.Intn(25)+10000)
+			time.Sleep(time.Second * amt)
 			t1 := time.Now()
-			log.Println("Running for " + t1.Sub(t0).String())
-			//	fmt.Println(database.View("user", "test@example.com", &user))
+			log.Println("Info: Running for " + t1.Sub(t0).String())
+		}
+	}()
+
+
+	// Test push notification (for site-wide announcements to all online users)
+	go func() {
+		for {
+			amt := time.Duration(rand.Intn(250))
+			time.Sleep(time.Second * amt)
+			log.Println("Info: Pushing notification to all online users ")
+			session.NotifyUsers("Breaking News: hello world.")
 		}
 	}()
 
@@ -108,7 +164,7 @@ func main() {
 		go func() {
 			for range c {
 
-				log.Printf("Starting Maintainance Routine")
+				log.Printf("Maint: Starting Maintainance Routine")
 
 				// Database Backup
 				_, err := maint.RunSchedule()
@@ -118,27 +174,30 @@ func main() {
 					fmt.Println(err)
 				}
 				database.Connect(config.Database)
-				log.Println("Database Backup completed.")
+				log.Println("Maint: Database Backup completed.")
 
 				_, err = maint.BankTransfer()
 				if err != nil {
 					fmt.Println(err)
 				}
 				// Bank Transfer
-				log.Println("Bank Transfer completed.")
+				log.Println("Maint: Bank Transfer completed.")
 
 				// Bitcoin Sync
 				_, err = maint.BitcoinSync()
 				if err != nil {
 					fmt.Println(err)
 				}
-				log.Println("Bitcoin Sync completed.")
+				log.Println("Maint: Bitcoin Sync completed.")
 
 			}
 		}()
 
 	}()
-
+	fmt.Printf("Boot: Complete (%s)\n",time.Now().Format("2006-01-02 15:04:05"))
+	if !InitLog() {
+		panic("error")
+	}
 	// Start the listener
 	server.Run(route.LoadHTTP(), route.LoadHTTPS(), config.Server)
 }
@@ -169,14 +228,13 @@ func (c *configuration) ParseJSON(b []byte) error {
 
 // InitLog switches the log engine to a file, rather than stdout
 func InitLog() bool {
-	f, err := os.OpenFile("./debug.log", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0600)
+	f, err := os.OpenFile("logs/debug.log", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0600)
 	if err != nil {
 		log.Printf("error opening file: %v", err)
-		log.Fatal("Hint: touch ./debug.log, or chown/chmod it so that the deltanine process can access it.")
+		log.Fatal("Hint: touch ./debug.log, or chown/chmod it so that the "+os.Args[0]+" process can access it.")
 		return false
 	}
-	log.Println("Powered on. Switched to log file.")
+	fmt.Println("Switched to log file. Nothing more to see here.")
 	log.SetOutput(f)
-	log.Println("Powered on. Switched to log file.")
 	return true
 }
