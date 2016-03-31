17d774f5c05e44c63bee0fb469a24581321d801b
aerth <aerth@isupon.us>
2016-03-30 21:47:02 -0700

    Missing from repo

---
+++ components/view/plugin/ricebox.go
@@ -0,0 +1,36 @@
+package plugin
+//
+// import (
+//
+//   "github.com/GeertJohan/go.rice"
+//   "log"
+//   "bytes"
+//   "io"
+//   "path"
+//   "path/filepath"
+// )
+//
+// type TemplateLunchbox struct {
+//   box *rice.Box
+// }
+//
+// func NewTemplateLoader() (*TemplateLunchbox, error) {
+// fs := &TemplateLunchbox()
+//   found, err := rice.FindBox("templates")
+//   if err != nil {
+//     return nil, err
+//   }
+//   fs.box = found
+//   return fs, nil
+// }
+//
+//
+//
+//
+// // get file contents as string
+// _, err := templateBox.String(tmpl)
+// if err != nil {
+//     log.Fatal(err)
+// }
+//
+// func Template
